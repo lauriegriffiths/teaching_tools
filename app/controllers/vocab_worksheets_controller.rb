9d40d0e3884436a44fce55ef2ef9c25e07d2a81e
Laurie Griffiths <laurieinjapan@gmail.com>
2015-12-11 13:12:48 +0900

    flesh out the vocab worksheet controller

--- app/controllers/vocab_worksheets_controller.rb
+++ app/controllers/vocab_worksheets_controller.rb
@@ -2,4 +2,30 @@ class VocabWorksheetsController < ApplicationController
   def new
     @vocab_worksheet = VocabWorksheet.new
   end
+
+  def create
+    @vocab_worksheet = VocabWorksheet.create(vocab_worksheet_params)
+    @vocab_worksheet.link_to_words
+    render 'edit' 
+  end
+
+  def update
+    @vocab_worksheet = VocabWorksheet.find(params[:id])
+    @vocab_worksheet.update_attributes(vocab_worksheet_params)
+    @vocab_worksheet.link_to_words
+    render 'edit'
+  end
+
+  def show
+    @vocab_worksheet = VocabWorksheet.find(params[:id])
+  end
+
+  def show
+    @vocab_worksheet = VocabWorksheet.find(params[:id])
+  end
+
+  private
+  def vocab_worksheet_params
+    params.require(:vocab_worksheet).permit(:title, :body)
+  end
 end
--- app/models/vocab_worksheet.rb
+++ app/models/vocab_worksheet.rb
@@ -1,4 +1,9 @@
 class VocabWorksheet < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :words
+
+  def link_to_words
+    words_in_body = Word.where(headword: body.split.uniq)
+    self.words = words_in_body
+  end
 end
