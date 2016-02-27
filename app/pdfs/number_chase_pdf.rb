f4e7fed6b62995311880a1cd1ee6c342faa3fb59
Laurie Griffiths <laurieinjapan@gmail.com>
2016-02-27 16:39:28 +0900

    fixed number chase page size

--- app/pdfs/number_chase_pdf.rb
+++ app/pdfs/number_chase_pdf.rb
@@ -1,9 +1,9 @@
 class NumberChasePdf < Prawn::Document
   def initialize(game)
-    super()
+    super(page_size: 'A4')
     game.number_pairs.each do |player|
       table_data = [["<b>hear</b>","<b>say</b>"]] + player
-      table(table_data,width:540, :row_colors => ["E0E0E0", "FFFFFF"],
+      table(table_data,width:530, :row_colors => ["E0E0E0", "FFFFFF"],
             :cell_style => {padding: 10, size:14, align: :center, inline_format: true})
       move_down 20
     end
